a32314e834ef43dde4cf7948dfc7532922d0835d
Андрей <andrey_y4in_1297@bk.ru>
2019-04-11 14:56:04 +0400

    Третий урок

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 const switcher = document.querySelector('#cbx'),
     more = document.querySelector('.more'),
     modal = document.querySelector('.modal'),
-    videos = document.querySelectorAll('.videos__item');
+    videos = document.querySelectorAll('.videos__item'),
+    videosWrapper = document.querySelector('.videos__wrapper');
 let player;
 
 function bindSlideToggle(trigger, boxBody, content, openClass) {
@@ -74,7 +75,7 @@ switcher.addEventListener('change', () => {
 })
 
 
-const data = [
+/*const data = [
     ['img/thumb_3.webp', 'img/thumb_4.webp', 'img/thumb_5.webp'],
     ['#3 Верстка на flexbox CSS | Блок преимущества и галерея | Марафон верстки | Артем Исламов',
         '#2 Установка spikmi и работа с ветками на Github | Марафон вёрстки Урок 2',
@@ -82,9 +83,9 @@ const data = [
     ],
     ['3,6 тыс. просмотров', '4,2 тыс. просмотров', '28 тыс. просмотров'],
     ['X9SmcY3lM-U', '7BvHoh0BrMw', 'mC8JW_aG2EM']
-];
+];*/
 
-more.addEventListener('click', () => {
+/*more.addEventListener('click', () => {
     const videosWrapper = document.querySelector('.videos__wrapper');
     more.remove();
 
@@ -105,7 +106,120 @@ more.addEventListener('click', () => {
         setTimeout(() => {
             card.classList.remove('videos__item-active');
         }, 10);
+        if(night === true) {
+            card.querySelector('.videos__item-descr').style.color = '#fff';
+            card.querySelector('.videos__item-views').style.color = '#fff';
+        }
+
+        //bindNewModal(card);
     }
+});*/
+
+function load() {
+    gapi.client.init({
+        'apiKey': 'AIzaSyA8AG6TkNEl8eatBsxBR0R23NtGn3OOxhw',
+        'discoveryDocs': ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"],
+    }).then(function() {
+        return gapi.client.youtube.playlistItems.list({
+            'part': 'snippet, contentDetails',
+            'maxResults': '6',
+            'playlistID': 'PL3LQJkGQtzc4gsrFkm4MjWhTXhopsMgpv'
+        })
+    }).then(function(response) {
+        console.log(response.result);
+        
+
+        response.result.items.forEach(item => {
+            let card = document.createElement('a');
+
+            card.classList.add('videos__item', 'videos__item-active');
+            card.setAttribute('data-url', item.contentDetails.videoId);
+
+            card.innerHTML = `
+            <img src="${item.snippet.thumbnails.high.url}" alt="thumb">
+            <div class="videos__item-descr">
+                ${item.snippet.title}
+            </div>
+            <div class="videos__item-views">
+                2.7 тыс. просмотров
+            </div>
+            `;
+            videosWrapper.appendChild(card);
+            setTimeout(() => {
+                card.classList.remove('videos__item-active');
+            }, 10);
+            if(night === true) {
+                card.querySelector('.videos__item-descr').style.color = '#fff';
+                card.querySelector('.videos__item-views').style.color = '#fff';
+            }  
+        })
+
+        sliceTitle('.videos__item-descr', 100);
+        bindModal(document.querySelectorAll('.videos__item'));
+
+    }).catch( e => {
+        console.log(e);
+    })
+}
+
+more.addEventListener('click', () => {
+    more.remove();
+    gapi.load('client', load);
+});
+
+function search(target) {
+    gapi.client.init({
+        'apiKey': 'AIzaSyA8AG6TkNEl8eatBsxBR0R23NtGn3OOxhw',
+        'discoveryDocs': ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"],
+    }).then(function() {
+        return gapi.client.youtube.search.list({
+            'maxResults': '10',
+            'part': 'snippet',
+            'q': `${target}`,
+            'type': ''
+        })
+    }).then(function(response) {
+        console.log(response.result);
+        //videosWrapper.innerHTML = '';
+
+        while(videosWrapper.firstChild) {
+            videosWrapper.removeChild(videosWrapper.firstChild);
+        }
+
+        response.result.items.forEach(item => {
+            let card = document.createElement('a');
+
+            card.classList.add('videos__item', 'videos__item-active');
+            card.setAttribute('data-url', item.id.videoId);
+
+            card.innerHTML = `
+            <img src="${item.snippet.thumbnails.high.url}" alt="thumb">
+            <div class="videos__item-descr">
+                ${item.snippet.title}
+            </div>
+            <div class="videos__item-views">
+                2.7 тыс. просмотров
+            </div>
+            `;
+            videosWrapper.appendChild(card);
+            setTimeout(() => {
+                card.classList.remove('videos__item-active');
+            }, 10);
+            if(night === true) {
+                card.querySelector('.videos__item-descr').style.color = '#fff';
+                card.querySelector('.videos__item-views').style.color = '#fff';
+            }  
+        })
+
+        sliceTitle('.videos__item-descr', 100);
+        bindModal(document.querySelectorAll('.videos__item'));
+    })
+}
+
+document.querySelector('.search').addEventListener('submit', (e) => {
+    e.preventDefault();
+    gapi.load('client', () => { search(document.querySelector('.search > input').value)});
+    document.querySelector('.search > input').value = '';
 });
 
 function sliceTitle(selector, count) {
@@ -141,7 +255,7 @@ function bindModal(cards) {
         });
     });
 }
-bindModal(videos);
+//bindModal(videos);
 
 function bindNewModal(cards) {
         item.addEventListener('click', (e) => {
@@ -156,7 +270,10 @@ modal.addEventListener('click', (e) => {
     if (!e.target.classList.contains('modal__body')) {
         closeModal();
     }
-})
+});
+/*document.addEventListener('keydown', function(evt) {
+
+});*/
 
 function createVideo() {
     var tag = document.createElement('script');
@@ -178,3 +295,5 @@ createVideo();
 function loadVideo(id) {
     player.loadVideoById({'videoId': `${id}`});
 }
+
+//AIzaSyA8AG6TkNEl8eatBsxBR0R23NtGn3OOxhw Ключ API
